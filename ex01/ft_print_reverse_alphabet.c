c6fda17e6d1a0705ed487f334f4a94c88d385e6f
Damien Abeloos <dabeloos@e-r6-p1.s19.be>
2018-08-09 11:40:34 +0200

    ex01 ft_print_reverse_alphabet.c

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/09 11:38:52 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+	char converter;
+	int count;
+	count = 25;
+	while(count >= 0)
+	{
+		converter = 'a' + count;
+		ft_putchar(converter);
+		count--;
+	}
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
