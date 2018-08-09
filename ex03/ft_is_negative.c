39ca1baee49bcde8dc329fdc1063383ddb9f5282
Damien Abeloos <dabeloos@e-r6-p1.s19.be>
2018-08-09 15:28:05 +0200

    corr ex03

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,17 +6,13 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 12:19:36 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/09 15:27:46 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
+int ft_putchar(char c);
 
 void ft_is_negative(int n)
 {
@@ -28,14 +24,4 @@ void ft_is_negative(int n)
 	{
 		ft_putchar('P');
 	}
-}
-
-int main()
-{
-	int n;
-	n = -1;
-	ft_is_negative(n);
-	n = 1;
-	ft_is_negative(n);
-	return (0);
 }
