ec42fefdb111c59f545f6266614744fdce50f6d0
Damien Abeloos <dabeloos@e-r6-p1.s19.be>
2018-08-09 12:17:55 +0200

    ex03 ft_is_negative.c

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/09 12:15:10 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
+	}
+}
+
+int main()
+{
+	int n;
+	n = -1;
+	ft_is_negative(n);
+	n = 1;
+	ft_is_negative(n);
+	return (0);
+}
