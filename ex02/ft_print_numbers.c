2b32228944081d2dab2364f7aea9e5cb02d0aa23
Damien Abeloos <dabeloos@e-r6-p1.s19.be>
2018-08-09 11:50:42 +0200

    ex02 ft_print_numbers

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/09 11:48:42 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_print_numbers(void)
+{
+	char converter;
+	int count;
+	count = 0;
+	while(count < 10)
+	{
+		converter = '0' + count;
+		ft_putchar(converter);
+		count++;
+	}
+}
+
+int main()
+{
+	ft_print_numbers();
+	return (0);
+}
