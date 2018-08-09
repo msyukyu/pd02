18c40e9205dc32f7fcc7a0f7f784b33d1ddeda63
Damien Abeloos <dabeloos@e-r6-p1.s19.be>
2018-08-09 18:40:47 +0200

    ex06 ft_putnbr

---
+++ ex06/ft_putnbr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 16:25:36 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/09 18:39:59 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_putchar(char c);
+
+void ft_putnbr(int nb)
+{
+	int divisor;
+	int digit;
+	divisor = 1000000000;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+	}
+	while (divisor > 0)
+	{
+		digit = (nb / divisor) % 10;
+		if (nb < 0)
+		{
+			digit *= - 1;
+		}
+		ft_putchar(digit + '0');
+		if (divisor == 1)
+		{
+			divisor = 0;
+		}
+		else
+		{
+			divisor /= 10;
+		}
+	}
+}
