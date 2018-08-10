c821b138e156bfe70253a9be285e2a3671c9e415
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-10 09:25:55 +0200

    ex02 norm

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,23 +6,24 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 15:25:34 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/10 09:25:36 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c);
+int		ft_putchar(char c);
 
-void ft_print_numbers(void)
+void	ft_print_numbers(void)
 {
-	char converter;
-	int count;
+	char	converter;
+	int		count;
+
 	count = 0;
 	while (count < 10)
 	{
 		converter = '0' + count;
 		ft_putchar(converter);
-		count ++;
+		count++;
 	}
 }
