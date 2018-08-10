f74d485dc22cf2062597013192e2e74a54cfbde9
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-10 12:10:21 +0200

    ex07 finished

--- ex07/ft_print_combn.c
+++ ex07/ft_print_combn.c
@@ -6,65 +6,80 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 19:08:27 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 20:37:27 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/10 12:09:21 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+int		ft_putchar(char c);
 
-int ft_putchar(char c)
+void	ft_display_digits(int digits[], int usable_size)
 {
-	write(1, &c, 1);
-	return (0);
-}
+	int		n;
 
-void ft_print_combn(int n)
-{
-	int crow;
-	int cvalue;
-	int lmrow;
-	int lmvalue;
-	bool first;
-	first = true;
-	crow = 0;
-	cvalue = 0;
-	mrow = 0;
-	mvalue = 0;
-	while ( ! (mrow == 0 && mvalue == 9 - n + 1))
+	n = 0;
+	if (!(digits[0] == 0 && digits[usable_size - 1] == usable_size - 1))
 	{
-		if (first)
-		{
-			first = false;
-		}
-		else
-		{
-			ft_putchar(',');
-			ft_putchar(' ');
-		}
-		while (crow < n)
-		{
-			/**
-			 * retenir la valeur de la colonne precedente au cas ou on
-			 * adapte avec mvalue. a faire dans le while
-			 **/
-			if (crow == mrow)
-			{
-				cvalue = mvalue;
-			}
-			ft_putchar(cvalue + '0');
-			if (crow + 1 == n)
-			{
-
-			}
-			crow ++;
-		}
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+	while (n < usable_size)
+	{
+		ft_putchar(digits[n] + '0');
+		n++;
 	}
 }
 
-int main()
+void	ft_fill_array(int digits[], int usable_size)
 {
-	int n;
-	n = 9;
-	ft_print_combn(n);
-	return (0);
+	int		i;
+
+	i = 0;
+	while (i < 9)
+	{
+		if (i < usable_size)
+		{
+			digits[i] = i;
+		}
+		else
+		{
+			digits[i] = -1;
+		}
+		i++;
+	}
+}
+
+int		ft_compute_min_value(int digits[], int i)
+{
+	int		min;
+	int		j;
+
+	j = i;
+	min = 9;
+	while (j > 0)
+	{
+		if (min > digits[j - 1] + i - (j - 1) + 1)
+			min = digits[j - 1] + i - (j - 1) + 1;
+		j = j - 1;
+	}
+	return (min);
+}
+
+void	ft_print_combn(int n)
+{
+	int		digits[9];
+	int		i;
+
+	ft_fill_array(digits, n);
+	i = n - 1;
+	while (digits[0] != 10 - n + 1)
+	{
+		ft_display_digits(digits, n);
+		while (digits[i] + 1 > 10 - n + i && i > 0)
+		{
+			digits[i] = ft_compute_min_value(digits, i);
+			i = i - 1;
+		}
+		digits[i] += 1;
+		i = n - 1;
+	}
 }
