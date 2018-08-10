18477ff1ca9821e59f030a1bfbde924414913753
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-10 09:28:00 +0200

    ex05 norm

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -6,17 +6,17 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 15:37:49 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 16:20:21 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/10 09:27:45 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c);
+int		ft_putchar(char c);
 
-void ft_display_digits_pair(int digits_0, int digits_1)
+void	ft_display_digits_pair(int digits_0, int digits_1)
 {
-	if( ! (digits_0 == 0 && digits_1 == 1))
+	if (!(digits_0 == 0 && digits_1 == 1))
 	{
 		ft_putchar(',');
 		ft_putchar(' ');
@@ -28,10 +28,11 @@ void ft_display_digits_pair(int digits_0, int digits_1)
 	ft_putchar((digits_1 % 10) + '0');
 }
 
-void ft_print_comb2(void)
+void	ft_print_comb2(void)
 {
-	int digits_0;
-	int digits_1;
+	int	digits_0;
+	int	digits_1;
+
 	digits_0 = 0;
 	digits_1 = 1;
 	while (digits_0 < 100)
@@ -39,9 +40,9 @@ void ft_print_comb2(void)
 		while (digits_1 < 100)
 		{
 			ft_display_digits_pair(digits_0, digits_1);
-			digits_1 ++;
+			digits_1++;
 		}
-		digits_0 ++;
+		digits_0++;
 		digits_1 = digits_0 + 1;
 	}
 }
