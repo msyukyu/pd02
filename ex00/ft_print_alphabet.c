66978db2e546213f98e9e72bfb1223ac185b8631
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-10 09:21:38 +0200

    ex00 norm

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,23 +6,24 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 09:54:23 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 15:17:53 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/10 09:21:11 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c);
+int		ft_putchar(char c);
 
-void ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
-	char converter;
-	int count;
+	char	converter;
+	int		count;
+
 	count = 0;
 	while (count < 26)
 	{
 		converter = 'a' + count;
 		ft_putchar(converter);
-		count ++;
+		count++;
 	}
 }
