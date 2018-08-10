ede20b25d811a03bc3f1ef2dd008f44c4f40d71e
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-10 09:15:49 +0200

    ex04 norm

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,17 +6,17 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 13:39:46 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/09 15:32:12 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/10 09:15:28 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c);
+int		ft_putchar(char c);
 
-void ft_display_digits(int digit_0, int digit_1, int digit_2)
+void	ft_display_digits(int digit_0, int digit_1, int digit_2)
 {
-	if ( ! (digit_0 == 0 && digit_1 == 1 && digit_2 == 2))
+	if (!(digit_0 == 0 && digit_1 == 1 && digit_2 == 2))
 	{
 		ft_putchar(',');
 		ft_putchar(' ');
@@ -26,11 +26,12 @@ void ft_display_digits(int digit_0, int digit_1, int digit_2)
 	ft_putchar('0' + digit_2);
 }
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
 	int digit_0;
 	int digit_1;
 	int digit_2;
+
 	digit_0 = 0;
 	digit_1 = 1;
 	digit_2 = 2;
@@ -41,12 +42,12 @@ void ft_print_comb(void)
 			while (digit_2 < 10)
 			{
 				ft_display_digits(digit_0, digit_1, digit_2);
-				digit_2 ++;
+				digit_2++;
 			}
-			digit_1 ++;
+			digit_1++;
 			digit_2 = digit_1 + 1;
 		}
-		digit_0 ++;
+		digit_0++;
 		digit_1 = digit_0 + 1;
 		digit_2 = digit_1 + 1;
 	}
